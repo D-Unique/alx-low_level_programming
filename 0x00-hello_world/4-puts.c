b5e2980404323ee243cf054fe114cf23b1b22da2
Emma <emmunigwe@gmail.com>
2022-09-08 21:44:29 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-\**
-* main = entry point
-* description print string followed by new line
-* Return: Always 0 
-*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
