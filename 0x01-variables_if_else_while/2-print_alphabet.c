6288c9fca96029323fae737c7c3f1c4a6c8150de
Emma <emmunigwe@gmail.com>
2022-09-09 21:40:29 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - this is the main function
+*Return: return function
+*/
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+		putchar('\n');
+		return (0);
+}
+
