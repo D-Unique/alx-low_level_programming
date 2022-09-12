89f6dfc139234fc192459a21edb142c4cfcd4eee
Emma <emmunigwe@gmail.com>
2022-09-12 03:34:52 -0700

    f

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * Description: prints Dora's quote
  * Return: Always 1 (Success)
  */
-int main(void)
+int main()
 {
 	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
