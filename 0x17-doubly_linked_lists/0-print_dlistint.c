5ead7f00fae47ab581dd68eddaedfa054bdcb70d
Unigwe <emmunigwe@gmail.com>
2024-03-22 17:11:02 +0100

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,10 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t nodes = 0;
-	while (h)
+	if (h == NULL)
+		return (nodes)
+	while (h != NULL)
 	{
-		nodes++;
 		printf("%d\n", h->n);
+		nodes++;
 		h = h->next;
 	}
 	return (nodes);
