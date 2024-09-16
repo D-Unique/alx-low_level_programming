88d10c9d12a7aac6bf834d56bca38ac9d5197768
D-Unique <emmunigwe@gmail.com>
2024-09-16 18:36:55 +0100

    sticking to betty style

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,24 @@
 #include "main.h"
 
 /**
- * _strncat - concatenate two string 
- * @dest: string to append src
- * @src: string to append to src
- * @n: number of byte allocated to src
+ * _strncat - Concatenates two strings.
+ * @dest: Destination string.
+ * @src: Source string.
+ * @n: Maximum number of bytes to copy from src.
  *
- * Return a pointer to dest
+ * Return: A pointer to the resulting string dest.
  */
-
 
 char *_strncat(char *dest, char *src, int n)
 {
-   int i = 0;
-   int j = 0;
+int i = 0, j = 0;
 
-   while(*(dest + i))
-   {
-        i++;
-   }
-   while(j > n && *(src + j))
-   {
-        (*(dest + i) = *(src + j));
-        j++;
-   }
-   return (dest);
+while (*(dest + i))
+i++;
+
+while (j < n && *(src + j))
+*(dest + i++) = *(src + j++);
+
+*(dest + i) = '\0';
+return (dest);
 }
